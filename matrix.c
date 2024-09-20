4e6551c9b30ebcc23bcf5141fb6f70f754dcf054
Olivier Henry <olivier.henry@gmx.com>
2024-09-20 16:12:40 +0200

    Implémentation de la trace, matrix_trace

--- matrix.c
+++ matrix.c
@@ -82,3 +82,15 @@ void matrix_print(FILE *f, matrix m)
     }
   }
 }
+
+float matrix_trace(matrix m) {
+  float res = 0.;
+  if (m.n1==m.n2) {
+    float res = 0.;
+    for (int i = 0; i<m.n1; i++) {
+      res += *matrix_get(m, i, i);
+    }
+    return res;
+  }
+  exit(53);
+}
